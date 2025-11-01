44dfa065059a544822ba0a0fa234c070b4acdbd4
Tanuj Guha Thakurta <tanuj414junat@gmail.com>
2025-11-01 22:34:01 +0000

    endpoint decorator fixed

--- backend/app/api/v1/endpoints/users.py
+++ backend/app/api/v1/endpoints/users.py
@@ -2,10 +2,12 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-app.get('/')
+@app.get('/')
 def landing_page():
     print("Hello world")
+    return "Hello world"
 
-app.get('/index')
+@app.get('/index')
 def index_page():
     print("Hello from index")
+    return "Hello from index"
