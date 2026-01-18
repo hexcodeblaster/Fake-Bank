26a4d441cbb54f3407e44e6f8fae22789c72b9a6
Tanuj Guha Thakurta <tanuj414junat@gmail.com>
2026-01-18 16:27:05 +0000

    async-fied

--- backend/app/api/v1/endpoints/accounts.py
+++ backend/app/api/v1/endpoints/accounts.py
@@ -11,20 +11,20 @@ account_router = APIRouter()
 
 
 @account_router.post("/create", response_model=AccountResponse, status_code=status.HTTP_201_CREATED)
-def create_account(account: AccountCreate, db: Session = Depends(get_db), account_service: AccountService = Depends(get_account_service)) -> AccountResponse:
+async def create_account(account: AccountCreate, db: Session = Depends(get_db), account_service: AccountService = Depends(get_account_service)) -> AccountResponse:
     created_account = account_service.create_account(db=db, account=account)
     return AccountResponse.model_validate(created_account)
 
 @account_router.put("/{account_id}/withdraw", response_model=AccountResponse)
-def withdraw(account_id: int, payload: AmountRequest, db: Session = Depends(get_db), account_service: AccountService = Depends(get_account_service)) -> AccountResponse:
+async def withdraw(account_id: int, payload: AmountRequest, db: Session = Depends(get_db), account_service: AccountService = Depends(get_account_service)) -> AccountResponse:
     withdrew_account = account_service.withdraw(account_id=account_id, amount=payload.amount, db=db)
     return AccountResponse.model_validate(withdrew_account)
 
 @account_router.put("/{account_id}/deposit", response_model=AccountResponse)
-def deposit(account_id: int, payload:AmountRequest, db: Session = Depends(get_db), account_service: AccountService = Depends(get_account_service)) -> AccountResponse:
+async def deposit(account_id: int, payload:AmountRequest, db: Session = Depends(get_db), account_service: AccountService = Depends(get_account_service)) -> AccountResponse:
     deposited_account = account_service.deposit(account_id=account_id, amount=payload.amount, db=db)
     return AccountResponse.model_validate(deposited_account)
 
 @account_router.get("/{account_id}/check")
-def check_balance(account_id: int, db: Session = Depends(get_db), account_service: AccountService = Depends(get_account_service)) -> Decimal:
+async def check_balance(account_id: int, db: Session = Depends(get_db), account_service: AccountService = Depends(get_account_service)) -> Decimal:
     return account_service.check_balance(account_id=account_id, db=db)
--- backend/app/api/v1/endpoints/users.py
+++ backend/app/api/v1/endpoints/users.py
@@ -12,27 +12,27 @@ user_router = APIRouter()
 
 
 @user_router.put("/create")
-def index_page(user: UserCreate, db: Session = Depends(get_db)):
+async def index_page(user: UserCreate, db: Session = Depends(get_db)):
     user_service = UserService()
     created_user = user_service.create_user(db=db, user=user)
     return created_user
 
 @user_router.get("/email/")
-def get_user_by_email(email: str, db: Session = Depends(get_db)):
+async def get_user_by_email(email: str, db: Session = Depends(get_db)):
     user_service = UserService()
     return user_service.get_user_by_email(db= db, email=email)
 
 @user_router.get("/all")
-def get_all_users(db: Session = Depends(get_db)):
+async def get_all_users(db: Session = Depends(get_db)):
     user_service = UserService()
     return user_service.get_all_users(db = db)
 
 @user_router.get("/{user_id}")
-def get_user_by_id(user_id: int, db: Session = Depends(get_db)):
+async def get_user_by_id(user_id: int, db: Session = Depends(get_db)):
     user_service = UserService()
     return user_service.get_user_by_id(db=db, user_id=user_id)
 
 @user_router.get("/{user_id}/accounts", response_model=list[AccountResponse])
-def get_user_accounts(user_id: int, db:Session = Depends(get_db)):
+async def get_user_accounts(user_id: int, db:Session = Depends(get_db)):
     accounts_service = AccountService()
     return accounts_service.get_accounts(user_id=user_id, db=db)
